70e2d8b1e8cf5be49ae9d52d1e099942cd4e6bd9
mutai-gilbert <mutaigilbert85@gmail.com>
2023-02-23 10:09:56 +0300

    Made changes to the index.js file

--- src/index.js
+++ src/index.js
@@ -1,46 +1,49 @@
 import './style.css';
 
-const tasks = [
-  {
-    description: 'Read the bible',
-    completed: true,
-    index: 0,
-  },
-  {
-    description: 'Read a book',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Attend Microverse',
-    completed: true,
-    index: 2,
-  },
-  {
-    description: 'Read MDN documentation',
-    completed: true,
-    index: 3,
-  },
-];
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+const theTodos = document.querySelector('.the-todos');
+
+function removeTasks() {
+  // remove the task from the dom
+  
+
+}
 
 function displayTasks() {
-  const theTodos = document.querySelector('.the-todos');
   theTodos.innerHTML = '';
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
   tasks.forEach((task) => {
     const li = document.createElement('li');
-    li.innerHTML = `
-    <li class="todos">
-    <div>
-        <i class="bi bi-app"></i>
-        <span> ${task.description}</span>
-    </div>
-    <div class="three-dots">
-        <i class="bi bi-three-dots-vertical"></i>
-    </div>
+    li.classList.add('todos');
 
-</li>
-      `;
+    const firstDiv = document.createElement('div');
+
+    const iElement = document.createElement('i');
+    iElement.classList.add('bi', 'bi-app');
+
+    const contentSpan = document.createElement('span');
+    contentSpan.innerText = task.description;
+
+    const secondDiv = document.createElement('div');
+    secondDiv.classList.add('three-dots');
+
+    const secondElement = document.createElement('i');
+    secondElement.classList.add('bi', 'bi-three-dots-vertical');
+    firstDiv.appendChild(iElement);
+    firstDiv.appendChild(contentSpan);
+    li.appendChild(firstDiv);
+    secondDiv.appendChild(secondElement);
+    li.appendChild(secondDiv);
     theTodos.appendChild(li);
   });
 }
-displayTasks();
+
+// function to add a new book
+const form = document.querySelector('#add-your-list');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const description = form.todo.value;
+  tasks.push({ description });
+  displayTasks();
+});
